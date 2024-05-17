bf98d1b2208065d502f8dde0b4544096caff63f4
Binayak17 <binayakmohanta0005@gmail.com>
2024-05-17 17:34:14 +0530

    Last version

--- server.js
+++ server.js
@@ -5,8 +5,36 @@ const db = require('./db')
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-app.get('/', function (req, res){
+//Middleware Function
+const logRequest = (req, res, next) => {
+    console.log(`[${new Date().toLocaleString()}] Request Made to ${req.originalUrl}`);
+    next();
+}
+app.use(logRequest);
+
+passport.use( new LocalStrategy(async (USERNAME, PASSWORD, done) => {
+    // authentication logic
+    try{
+        const user = await Person.findOne({username : USERNAME});
+        if(!user)
+            return done(null, false, {message: 'Incorrect username'});
+        const isPasswordMatch = PASSWORD === user.password;
+        if(!isPasswordMatch)
+            return done(null, user);
+        else
+            return done(null, false, {message: 'Incorrect password'});
+    }catch(err){
+        return done(err);
+    }
+    
+}))
+
+app.use(passport.initialize());
+
+app.get('/',passport.authenticate('local', {session: false}) ,function (req, res){
     res.send('Welcome')
 })
 
